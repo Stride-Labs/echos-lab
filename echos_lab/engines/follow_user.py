893ba9dd13e9ee2da4fdcdecfc6ff1ed5b5b69b6
sampocs <sam.pochyly@gmail.com>
2025-01-02 17:41:50 -0600

    removed follow user file

--- echos_lab/engines/follow_user.py
+++
@@ -1,9 +0,0 @@
-from twitter.account import Account
-
-
-def follow_user(account: Account, user_id):
-    return account.follow(user_id)
-
-
-def like_post(account: Account, post_id):
-    return account.like(post_id)
